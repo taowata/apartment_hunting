dc9fa0156d0c0081954cecbabcbb4df5011bdc4a
Ossamoon <osamu950623@gmail.com>
2020-11-23 18:25:24 +0900

    create login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import registerfunc
+from .views import register_func, login_func
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 app_name = 'accounts'
 urlpatterns = [
-    path('register/', registerfunc),
+    path('register/', register_func),
+    path('login/', login_func),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
