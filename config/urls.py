4fb29768e8b533f28f3b66f09dfae8646c1ca5eb
Ossamoon <osamu950623@gmail.com>
2020-11-29 21:57:26 +0900

    fix appearance of logout-page, make the pass "/" to connect login page

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path('apartment/', include('apartment.urls')),
     path('records/', include('import_records.urls')),
 ]
