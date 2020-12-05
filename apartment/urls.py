e18d2f7e3af1d5a8dec86c2d1dbc8f0e642b6427
Ossamoon <osamu950623@gmail.com>
2020-12-05 18:09:11 +0900

    made carousels which show room-images

--- apartment/urls.py
+++ apartment/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ApartmentListView
+from .views import ApartmentListView, detail_func
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 app_name = 'apartment'
 urlpatterns = [
     path('list', ApartmentListView.as_view()),
+    path('detail/<int:pk>', detail_func, name='detail')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- apartment/views.py
+++ apartment/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Apartment
+from .models import Apartment, Room
 
 
 # Create your views here.
@@ -10,3 +11,8 @@ class ApartmentListView(LoginRequiredMixin, ListView):
     template_name = "apartment/list.html"
     model = Apartment
     context_object_name = 'apartment_list'
+
+
+def detail_func(request, pk):
+    room_object = Room.objects.get(pk=pk)
+    return render(request, 'apartment/detail.html', {'room_object': room_object})
